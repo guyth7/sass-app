0b64574050604447c3b40240a1199c6e6d9fd37e
Guyth <guyth.daba@gamil.com>
2026-03-14 01:13:44 +0300

    feat: add multiple new pages for companion sessions, profile, sign-in, and subscription

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <h1 className="underline text-2xl">Welcome to my SasS Application</h1>
+      <Button>Let&apos;s get started</Button>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
